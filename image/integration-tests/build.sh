f6ba82a0e7a4ac94c9d8bf3b876e10cea6eb4a47
Chris Bevard <cbevard1@gmail.com>
2021-09-24 09:22:26 -0400

    fixed build image

--- image/integration-tests/build.sh
+++ image/integration-tests/build.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr.us-east-2.amazonaws.com
-docker build -t as3-integration-tests:latest .
-docker tag as3-integration-tests:latest  "$AWS_ACCOUNT_ID".dkr.ecr.us-east-2.amazonaws.com/as3-integration-tests:latest
-docker push "$AWS_ACCOUNT_ID".dkr.ecr.us-east-2.amazonaws.com/as3-integration-tests:latest
+docker build -t acc-s3-intg:latest .
+docker tag acc-s3-intg:latest  "$AWS_ACCOUNT_ID".dkr.ecr.us-east-2.amazonaws.com/acc-s3-intg:latest
+docker push "$AWS_ACCOUNT_ID".dkr.ecr.us-east-2.amazonaws.com/acc-s3-intg:latest
