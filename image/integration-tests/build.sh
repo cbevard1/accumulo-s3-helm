71230e6ec3e5cfc80df75fa4a696d7ff577ad16b
Chris Bevard <cbevard1@gmail.com>
2021-11-09 20:58:41 -0500

    testing 1.9.3

--- image/integration-tests/build.sh
+++ image/integration-tests/build.sh
@@ -2,4 +2,5 @@ tag=$1
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr.us-east-2.amazonaws.com
 docker build --no-cache -t acc-s3-intg:"$tag" .
 docker tag acc-s3-intg:"$tag"  "$AWS_ACCOUNT_ID".dkr.ecr.us-east-2.amazonaws.com/acc-s3-intg:"$tag"
+docker tag acc-s3-intg:"$tag"  "$AWS_ACCOUNT_ID".dkr.ecr.us-east-2.amazonaws.com/acc-s3-intg:"$tag"
 docker push "$AWS_ACCOUNT_ID".dkr.ecr.us-east-2.amazonaws.com/acc-s3-intg:"$tag"
