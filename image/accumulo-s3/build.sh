ede0c9197ef99e18d83d60b7e0da24b8574cfecb
Chris Bevard <cbevard1@gmail.com>
2022-01-12 13:45:27 -0500

    updated chart to follow the config changes made in accumulo 2.1.0

--- image/accumulo-s3/build.sh
+++ image/accumulo-s3/build.sh
@@ -1,5 +1,5 @@
 tag=$1
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 347653770938.dkr.ecr.us-east-2.amazonaws.com
-docker build --no-cache -t accumulo-s3:"$tag" .
+docker build -t accumulo-s3:"$tag" .
 docker tag accumulo-s3:"$tag"  347653770938.dkr.ecr.us-east-2.amazonaws.com/accumulo-s3:"$tag"
 docker push 347653770938.dkr.ecr.us-east-2.amazonaws.com/accumulo-s3:"$tag"
